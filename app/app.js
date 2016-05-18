8e6c703bf5b42ee3ebe8472747be3f49d0b9d140
Julian Stiven Rivera <jlr527@hotmail.com>
2016-05-17 23:42:44 -0500

    cambios

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-var app=angular.module(function('app',['ngRoute']){});
+var app=angular.module(function('app',['ngRoute']){}).config(function ($routeProvider, $locationProvider) {
+
+        $routeProvider.when('/', {
+            templateUrl: '/app/view/home.html',
+            controller: 'IndexController'
+        }).otherwise({ redirectTo: '/' });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
