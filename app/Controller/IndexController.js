81ba7c373fa00a87eaf95624286a45a9e8bd6071
Yair Arango <eyairarango@gmail.com>
2016-05-20 13:45:55 -0500

    Validacion de chip simbolos

--- app/Controller/IndexController.js
+++ app/Controller/IndexController.js
@@ -1,8 +1,22 @@
-app.controller('IndexController', function($scope, VisDataSet,tableService) {
+app.controller('IndexController', function($scope, VisDataSet, $timeout,tableService) {
     
 
     $scope.simbolos = [];
     $scope.estados = [];
+    $scope.table=[{}];
+    $scope.info=[];
+    $scope.onKeyPressResult="";
+    $scope.count;
+    //var table={columnas:[{}],registros:[{}]};
+
+    
+    angular.forEach($scope.estados, function (value, key) {
+        $scope.table[key]['Estados'] = value;
+        angular.forEach($scope.simbolos, function (val, ke) {
+            $scope.table[key][val] = $scope.info[key][val];
+        });
+
+    });
 
     $scope.newEstado = function(chip) {
         return {
@@ -10,17 +24,29 @@ app.controller('IndexController', function($scope, VisDataSet,tableService) {
         };
     };
 
-    $scope.newSimbolo = function(chip) {
+    
+    $scope.newSimbolo = function(chip,count) {
+    if($scope.onKeyPressResult>33 && count==1){
+        $scope.count=0;
+        console.log("hola");
         return {
             name: chip
-        };
+        };        
+    }else { 
+        chip="";   
+       //$scope.simbolos.pop();
+        $scope.count=0;
+        return null;}
+       //console.log($scope.count);
     };
 
-    tableService.setColumns($scope.simbolos);
+    $scope.myItems=[];
+
+    /*tableService.setColumns($scope.simbolos);
     tableService.setEstados($scope.estados);
 
     $scope.table=tableService.getTable();
-    $scope.esta=tableService.getEstados();
+    $scope.esta=tableService.getEstados();*/
 // create an array with nodes
     var nodos = VisDataSet([
 	  {id: 1, label: "A"},
@@ -79,7 +105,98 @@ app.controller('IndexController', function($scope, VisDataSet,tableService) {
         contextmenu: $scope.rightClick
     };
 
-    // initialize your network!
-   // var network = new vis.Network(container, data, options);
+    //initilization of the table 
 
+    //var simbolos=tableService.getColumns();
+    //tableService.setTable($scope.data);
+    //var data=tableService.getTable();
+    var data= $scope.table;
+    $timeout(function(){
+      $scope.loaded = true;
+    },1500);
+      /*
+      $scope.data = [{
+        "name": "Lukas",
+        "surname": "Torek",
+        "phone": "666-444-333"
+      }, {
+        "name": "Jeremy",
+        "surname": "Zbyszko",
+        "phone": "636-434-333"
+      },{
+        "name": "Marty",
+        "surname": "Wariat",
+        "phone": "636-434-333"
+      }, {
+        "name": "Tom",
+        "surname": "Smith",
+        "phone": "444-333-222"
+      },
+                    {
+        "name": "Lukas",
+        "surname": "Torek",
+        "phone": "666-444-333"
+      }, {
+        "name": "Jeremy",
+        "surname": "Zbyszko",
+        "phone": "636-434-333"
+      },{
+        "name": "Marty",
+        "surname": "Wariat",
+        "phone": "636-434-333"
+      }, {
+        "name": "Tom",
+        "surname": "Smith",
+        "phone": "444-333-222"
+      },{
+        "name": "Lukas",
+        "surname": "Torek",
+        "phone": "666-444-333"
+      }, {
+        "name": "Jeremy",
+        "surname": "Zbyszko",
+        "phone": "636-434-333"
+      },{
+        "name": "Marty",
+        "surname": "Wariat",
+        "phone": "636-434-333"
+      }, {
+        "name": "Tom",
+        "surname": "Smith",
+        "phone": "444-333-222"
+      }];*/
+/*
+  $scope.columns = [{
+          "name": "Your name",
+          "identificator": "name",
+          "settings": {
+            "sortable": true,
+          }
+        }, {
+          "name": "Surname"
+        }, {
+          "name": "Phone number",
+          "identificator": "phone",
+          "settings": {
+            "sortable": true,
+          }
+        }];*/
+    //simbolos.push({'name':'Aceptacion'});
+    $scope.dataset=data;
+    $scope.columns=$scope.simbolos;
+    //$scope.columns.push({'name':'Aceptacion'});
+
+
+var getKeyboardEventResult = function (keyEvent, keyEventDesc)
+    {
+      return window.event.keyCode;
+    };
+
+    $scope.estan = function ($event) {
+      $scope.onKeyPressResult = getKeyboardEventResult($event);  
+      if($scope.onKeyPressResult>33)
+      $scope.count ++; 
+    };
+
+    $scope.restar = function() { $scope.count = 0; };
 });
