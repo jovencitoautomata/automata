a04b65dbeb8973e8b8c2c415b6f299d7c9d89929
Julian Stiven Rivera <jlr527@hotmail.com>
2016-05-23 00:33:42 -0500

    casi fin

--- app/Controller/IndexController.js
+++ app/Controller/IndexController.js
@@ -1,12 +1,14 @@
-app.controller('IndexController', function($scope, VisDataSet, $timeout) {
+app.controller('IndexController', function($scope, VisDataSet, $timeout, $mdSidenav, $log) {
     $scope.simbolos = [];
     $scope.estados = [];
     $scope.info=[];
     $scope.onKeyPressResult="";
     $scope.count;
+    $scope.cadena="";
+    $scope.aceptacion;
     $scope.tra=[];
     $scope.removeChip=function(index,holder,chip){
-        //console.log(holder.$element[0].id);
+        
         if(holder.$element[0].id=="chipestados"){
             for(var $i=0;$i<$scope.tra.length; $i++) {
                 if($scope.tra[$i]['estado']==chip){
@@ -42,8 +44,6 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
         return null;}
     };
 
-    $scope.myItems=[];
-
 // create an array with nodes
 
     var nodos = VisDataSet([]);
@@ -51,7 +51,7 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
 
 
     $scope.$watch('estados',function(data){
-    //console.log(data);
+   
         var nodosGraf=[];
         angular.forEach($scope.estados,function(value, key){
             nodosGraf.push({id:value.name,label: value.name});
@@ -68,18 +68,26 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
     $scope.$watch('tra',function(data){
         var aris=[];
         angular.forEach($scope.tra,function(value,key){
+           
+            if(value.aceptacion)
+                nodos.update([{id:value.estado,color:{background:'rgb(105,240,174)'}}]);
+            
+            else
+                nodos.update([{id:value.estado,color:{background:'rgba(240,252,86,1)'}}]);
+            
+
             angular.forEach(value,function(val,ke){
                 if(ke!='estado' && ke!='aceptacion')
                 aris.push({from : value.estado, to : val, label : ke});
             });
         });
-        //console.log(aristas);
+        
         aristas=VisDataSet(aris);
         $scope.data = {
             nodes: nodos,
             edges: aristas
         };
-        console.log($scope.data);
+        
     },true);
 
 
@@ -89,7 +97,88 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
     // provide the data in the vis format
 
 
-    $scope.options = {};
+    $scope.options = {  "nodes": {
+    "color": {
+      "background": "rgba(240,252,86,1)"
+    },
+    "font": {
+      "color": "rgba(0,0,0,1)",
+      "size": 25,
+      "face": "tahoma",
+      "background": "rgba(76,76,76,0)"
+    },
+    "scaling": {
+      "min": 38,
+      "max": 58
+    },
+    "shape": "circle",
+    "size": 64
+  },
+  "edges": {
+    "arrows": {
+      "to": {
+        "enabled": true,
+        "scaleFactor": 0.55
+      }
+    },
+    "font": {
+      "color": "rgba(52,10,5,1)",
+      "size": 16,
+      "face": "tahoma",
+      "align": "top"
+    },
+    "scaling": {
+      "min": 17,
+      "max": 27
+    },
+    "shadow": {
+      "enabled": true
+    },
+    "smooth": {
+      "forceDirection": "none"
+    }
+  },
+  "interaction": {
+    "hover": true,
+    "multiselect": true,
+     "navigationButtons": true
+  },
+  "manipulation": {
+    "enabled": false,
+    "initiallyActive": true,
+    addNode: function (data, callback) {
+            $mdSidenav('nodos')
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + "nodos" + " is done");
+            $scope.nombreEst="";
+          });
+
+          },
+          editNode: function (data, callback) {
+            // filling in the popup DOM elements
+            document.getElementById('operation').innerHTML = "Edit Node";
+            document.getElementById('node-id').value = data.id;
+            document.getElementById('node-label').value = data.label;
+            document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
+            document.getElementById('cancelButton').onclick = cancelEdit.bind(this,callback);
+            document.getElementById('network-popUp').style.display = 'block';
+          },
+          addEdge: function (data, callback) {
+            if (data.from == data.to) {
+              var r = confirm("Do you want to connect the node to itself?");
+              if (r == true) {
+                callback(data);
+              }
+            }
+            else {
+              callback(data);
+            }
+          }
+  },
+  "physics": {
+    "minVelocity": 0.75
+  },"locale": "es"};/*
 
         $scope.onSelect = function (items) {
         // debugger;
@@ -120,6 +209,36 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
         doubleClick: $scope.onDoubleClick,
         contextmenu: $scope.rightClick
     };
+    */
+    //slide de insertar nodos
+
+    $scope.agregarNodo=function(){
+        var nodosGraf=[];
+        var existe=false;
+        angular.forEach($scope.estados,function(value, key){
+            if(value.name==$scope.nombreEst)
+                existe=true;
+            nodosGraf.push({id:value.name,label: value.name});
+        });
+        if(!existe){
+        nodosGraf.push({id:$scope.nombreEst,label:$scope.nombreEst});
+        $scope.estados.push({name:$scope.nombreEst});
+        }
+        nodos = VisDataSet(nodosGraf);
+        // create an array with edges
+
+        $scope.data = {
+            nodes: nodos,
+            edges: aristas
+        };
+    };
+    //cerar slide
+   $scope.close = function () {
+      $mdSidenav('nodos').close()
+        .then(function () {
+          $log.debug("close nodos is done");
+        });
+    };
 
     var getKeyboardEventResult = function (keyEvent, keyEventDesc)
         {
@@ -135,4 +254,32 @@ app.controller('IndexController', function($scope, VisDataSet, $timeout) {
     };
 
     $scope.restar = function() { $scope.count = 0; };
+
+    /*metodos mios*/
+    $scope.resultadoCadena=function(cadena){
+        var letras=[];
+        var inicial;
+        //var estado;
+        letras = cadena.split(""); 
+        inicial = $scope.tra[0][letras[0]];
+        //estado = $scope.tra[0]['aceptacion'];
+
+        for(var $i=1;$i<=letras.length; $i++) {
+            for(var $j=0;$j<$scope.tra.length;$j++){
+                if($i==letras.length){
+                    if($scope.tra[$j]['estado']==inicial){
+                        $scope.aceptacion=$scope.tra[$j]['aceptacion'];                        
+                        break;
+                    }
+                }else{
+                   if($scope.tra[$j]['estado']==inicial){
+                       inicial=$scope.tra[$j][letras[$i]];
+                       //estado=$scope.tra[$j]['aceptacion'];
+                       break;
+                   }
+                }
+            }
+        }
+
+    }
 });
